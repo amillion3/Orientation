ca5c524f0f477b698cace9ff4b2a2f5f577d64f0
Andy Million <amillion3@gmail.com>
2018-09-15 09:35:22 -0500

    initial setup and Department class

---
+++ Orientation/Department.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orientation
+{
+    class Department
+    {
+        public string _name { get; private set; }
+        public string _type { get; private set; }
+        public string _supervisor { get; private set; }
+        public int _employeeCount { get; private set; }
+        public bool _profitable { get; private set; }
+
+        public Department(string name, string type, string supervisor, int employeeCount, bool profitable)
+        {
+            _name = name;
+            _type = type;
+            _supervisor = supervisor;
+            _employeeCount = employeeCount;
+            _profitable = profitable;
+        }
+    }
+}
